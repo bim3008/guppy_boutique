9a058480c41d3d2b1cbfc2c3df69bcd939a83683
duynguyen3009 <dinhduy700@gmail.com>
2020-11-19 12:39:09 +0700

    front-end : cắt trang index.php, trang 404, porto-17

---
+++ document/template/html/copy-right.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <div class="footer-bottom">
+        <p class="footer-copyright">Porto eCommerce. &copy;  2018.  All Rights Reserved</p>
+        <img src="assets/images/payments.png" alt="payment methods" class="footer-payments">
+
+        <div class="social-icons">
+            <a href="#" class="social-icon" target="_blank"><i class="icon-facebook"></i></a>
+            <a href="#" class="social-icon" target="_blank"><i class="icon-twitter"></i></a>
+            <a href="#" class="social-icon" target="_blank"><i class="icon-linkedin"></i></a>
+        </div><!-- End .social-icons -->
+    </div><!-- End .footer-bottom -->
+</div><!-- End .containr -->
